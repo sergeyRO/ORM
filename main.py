65119e32d94fd0c86136314e1b10d328fccdfc8d
SergeyRO <sergey_r.o@mail.ru>
2022-07-26 21:45:00 +0300

    First

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from model import create_tables, Book, Publisher, Stock, Sale, Shop
+
+DSN = "postgresql://postgres:1986serG2909@localhost:5432/ORM_DB"
+engine = sqlalchemy.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with open('tests_data.json', 'r') as f:
+    data = json.load(f)
+for item in data:
+    model = {
+        'publisher': Publisher,
+        'book': Book,
+        'shop': Shop,
+        'stock': Stock,
+        'sale': Sale
+    }.get(item['model'])
+    session.add(model(id=item.get('pk'), **item.get('fields')))
+session.commit()
+
+print(session.query(Publisher).filter(Publisher.id == input('Введите ID издателя: ')).first())
+
+session.close()
